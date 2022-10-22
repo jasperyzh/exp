083eb72637babac5976d67baf10c7fdc5acd2dca
jasperyzh <jasper.yzh@gmail.com>
2022-10-22 19:02:28 +0800

    deploy github-pages

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # place .nojekyll to bypass Jekyll processing
 echo > .nojekyll
@@ -15,15 +15,18 @@ echo > .nojekyll
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -B main
-git add -A
-git commit -m 'deploy'
+# git init
+# git checkout -B main
+# git add -A
+git add .
+git commit -m 'deploy github-pages'
+git push
+git subtree push --prefix dist origin gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jasperyzh/exp.git main:gh-pages
+# git push -f git@github.com:jasperyzh/exp.git main:gh-pages
 
 cd -
